c94d28049b99ca4b3e6dcb78bb3b278d090c01a3
QiuHaoHao <qiuh0008@e.ntu.edu.sg>
2020-04-13 17:54:19 +0800

    Implement mr/master

---
+++ src/mr/utils.go
@@ -0,0 +1,12 @@
+package mr
+
+func prepWorkerReadyReply(untypedTask interface{}, reply *WorkerReadyReply) {
+	switch task := untypedTask.(type) {
+	case MapTask:
+		reply.MapTask = &task
+	case ReduceTask:
+		reply.ReduceTask = &task
+	default:
+		panic("unknown task type")
+	}
+}
